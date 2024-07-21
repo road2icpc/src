295f45fbb0fe5df1a570b6566ad4d9cd0f8d41ff
Slemmie <thorvejeneriksen@gmail.com>
2024-07-21 02:52:30 +0200

    add Setup/template.h

---
+++ src/Setup/template.h
@@ -0,0 +1,15 @@
+//# Title: template
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define vi vector <int>
+#define vl vector <long long>
+#define vvi vector <vector <int>>
+#define pii pair <int, int>
+#define siz(v) (int) (v).size()
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+}
