a163bd4f362f43bd4b9d31f9117c0bf6d0ef07e0
Slemmie <thorvejeneriksen@gmail.com>
2025-11-23 18:43:14 +0100

    NWERC2025 add missing pragma once

--- src_nwerc2025/Various/knuth_DP.h
+++ src_nwerc2025/Various/knuth_DP.h
@@ -11,3 +11,5 @@
 //# Description: [kactl] When doing DP on intervals: $a[i][j] = \min_{i < k < j}(a[i][k] + a[k][j]) + f(i, j)$, where the (minimal) optimal $k$ increases with both $i$ and $j$, one can solve intervals in increasing order of length, and search $k = p[i][j]$ for $a[i][j]$ only between $p[i][j-1]$ and $p[i+1][j]$. This is known as Knuth DP. Sufficient criteria for this are if $f(b,c) \le f(a,d)$ and $f(a,c) + f(b,d) \le f(a,d) + f(b,c)$ for all $a \le b \le c \le d$.
 //# Consider also: Line container, monotone queues, ternary search.
 //# Complexity: $\mathcal{O}(N^2)$
+
+#pragma once
