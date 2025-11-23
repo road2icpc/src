053d871ccb6e786804d0c59c7fb6578c1f34ec40
Slemmie <thorvejeneriksen@gmail.com>
2025-11-23 17:57:19 +0100

    NWERC2025 add kactl various chapter

---
+++ src_nwerc2025/Various/fast_knapsack.h
@@ -0,0 +1,33 @@
+/**
+ * Author: Mårten Wiman
+ * License: CC0
+ * Source: Pisinger 1999, "Linear Time Algorithms for Knapsack Problems with Bounded Weights"
+ * Description: Given N non-negative integer weights w and a non-negative target t,
+ * computes the maximum S <= t such that S is the sum of some subset of the weights.
+ * Time: O(N \max(w_i))
+ * Status: Tested on kattis:eavesdropperevasion, stress-tested
+ */
+
+//# Title: Fast knapsack
+//# Description: [kactl] Given N non-negative integer weights w and a non-negative target t, computes the maximum S <= t such that S is the sum of some subset of the weights.
+//# Complexity: $\mathcal{O}(N \max(w_i))$
+ 
+
+#pragma once
+
+int knapsack(vector <int> w, int t) {
+	int a = 0, b = 0, x;
+	while (b < sz(w) && a + w[b] <= t) a += w[b++];
+	if (b == sz(w)) return a;
+	int m = *max_element(all(w));
+	vi u, v(2*m, -1);
+	v[a+m-t] = b;
+	for (int i = b; i < (int) w.size(); i++) {
+		u = v;
+		for (int x = 0; x < m; x++) v[x+w[i]] = max(v[x+w[i]], u[x]);
+		for (x = 2*m; --x > m;) for (int j = max(0, u[x]); j < v[x]; j++)
+			v[x-w[j]] = max(v[x-w[j]], j);
+	}
+	for (a = t; v[a+m-t] < 0; a--) ;
+	return a;
+}
---
+++ src_nwerc2025/Various/unrolling.h
@@ -0,0 +1,18 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-03-19
+ * License: CC0
+ * Source: me
+ * Description:
+ */
+
+//# Title: Manual loop unrolling
+//# Description: [kactl] Manual loop unrolling.
+
+#pragma once
+
+#define F {...; ++i;}
+int i = from;
+while (i&3 && i < to) F // for alignment, if needed
+while (i + 4 <= to) { F F F F }
+while (i < to) F
