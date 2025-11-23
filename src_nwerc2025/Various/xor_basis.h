fc5315e665206c25fb1afa170df45cca8dcb6679
Slemmie <thorvejeneriksen@gmail.com>
2025-11-23 16:07:21 +0100

    NWERC2025 add xor_basis

---
+++ src_nwerc2025/Various/xor_basis.h
@@ -0,0 +1,15 @@
+//# Title: Xor basis
+//# Description: Basis of vectors in $Z^d_{2}$
+
+#pragma once
+
+struct XB {
+	vector<int> basis;
+	void ins(int mask) {
+		for(auto &y : basis) {
+			if(y < mask) swap(y, mask);
+			mask = min(mask, mask ^ y);
+		}
+		if(mask) basis.push_back(mask); // if mask is 0 value can already be represented by basis
+	}
+};
