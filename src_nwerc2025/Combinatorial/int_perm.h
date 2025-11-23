72d71a1265492000262def5d9497299330a75d86
AsbjornL <asbjorn.s.lind@gmail.com>
2025-11-23 15:27:42 +0100

    nwerc2025: Add combinatorials from kactl

---
+++ src_nwerc2025/Combinatorial/int_perm.h
@@ -0,0 +1,24 @@
+//@ Author: Simon Lindholm
+//@ Date: 2018-07-06
+//@ License: CC0
+//@ Description: Permutation -> integer conversion. (Not order preserving.)
+//@ Integer -> permutation can use a lookup table.
+//@ Time: O(n)
+
+//# Title: Permutation to Int
+//# Description: [kactl] Given a permutation, returns the number of lexicographically strictly smaller permutations.
+//# Complexity: O(n), but returns a value that is O(n!)
+
+//@ tested on:
+//@	1. nothing :(
+
+#pragma once
+
+int permToInt(vector<int> v) {
+	int use = 0, i = 0, r = 0;
+	for(int x : v) {
+		r = r * ++i + __builtin_popcount(use & -(1<<x));
+		use |= 1 << x;
+	}
+	return r;
+}
