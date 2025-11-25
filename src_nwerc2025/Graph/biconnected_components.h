2fe05bae0dd58d78d894949a7a724fe64f486895
Elias Lolck <sofapudr@gmail.com>
2025-11-25 16:12:49 +0100

    Updated biconnected_components to construct the block-cut tree

--- src_nwerc2025/Graph/biconnected_components.h
+++ src_nwerc2025/Graph/biconnected_components.h
@@ -1,54 +1,66 @@
-//@ Author: Simon Lindholm
-//@ Date: 2017-04-17
-//@ License: CC0
-//@ Source: folklore
-//@ Status: tested during MIPT ICPC Workshop 2017
-
 //# Title: Biconnected Components
-//# Description: [kactl] Finds all biconnected components in an undirected graph, and
-//#  runs a callback for the edges in each. In a biconnected component there
-//#  are at least two internally disjoint paths between any two nodes (a cycle
-//#  exists through them). Note that a node can be in several components. An
-//#  edge which is not in a component is a bridge, i.e., not part of any cycle.
+//# Description: Finds all biconnected components in an undirected graph, and
+//#  construct the block-cut tree of the graph. In a block-cut tree each node
+//#  correspond to a biconnected component or an articualtion point. Checking
+//#  if there is a path from u to v not going through w in the graph then 
+//#  correspond to checking if there exists a path from id[u] to id[v] in the
+//#  block-cut tree not going through id[w].
 //# Usage:
-//#  int eid = 0; ed.resize(N);
-//#  for each edge (a,b) \{
-//#    ed[a].emplace\_back(b, eid);
-//#    ed[b].emplace\_back(a, eid++); \}
-//#  bicomps([\&](const vi\& edgelist) \{ ... \});
+//#  G should be an adjacenylist with edges both ways
+//#  vector<bool> art;
+//#  vector<int> id;
+//#  auto T = bcomps(G, art, id)
+//#  then T is the block-cut tree.
 //# Complexity: $\mathcal{O}(E + V)$
 
 #pragma once
 
-vector<int> num, st;
-vector<vector<pair<int, int>>> ed;
-int time;
-template<class F>
-int dfs(int at, int par, F& f) {
-	int me = num[at] = ++Time, top = me;
-	for (auto [y, e] : ed[at]) if (e != par) {
-		if (num[y]) {
-			top = min(top, num[y]);
-			if (num[y] < me)
-				st.push_back(e);
-		} else {
-			int si = st.size();
-			int up = dfs(y, e, f);
-			top = min(top, up);
-			if (up == me) {
-				st.push_back(e);
-				f(vector<int>(st.begin() + si, st.end()));
-				st.resize(si);
+vector<vector<int>> bcomps(vector<vector<int>> &G, vector<bool> &art, vector<int> &id) {
+	int n = G.size();
+	vector<vector<int>> cmps, t;
+	vector<int> s, nm(n, 0), lw(n, 0);
+	art.resize(n);
+	id.resize(n);
+	int tt = 0;
+	auto f = [&](auto &&self, int u, int p) -> void {
+		lw[u] = nm[u] = ++tt;
+		s.push_back(u);
+		for(int v : G[u]) {
+			if(v == p) continue;
+			if(nm[v]) lw[u] = min(lw[u], nm[v]);
+			else {
+				self(self, v, u);
+				lw[u] = min(lw[u], lw[v]);
+				if(lw[v] >= nm[u]) {
+					art[u] = (nm[u] > 1 || nm[v] > 2);
+					cmps.push_back({u});
+					while(cmps.back().back() != v) {
+						cmps.back().push_back(s.back());
+						s.pop_back();
+					}
+				}
 			}
-			else if (up < me) st.push_back(e);
-			else { /* e is a bridge */ }
+		}
+	};
+	for(int i = 0; i < n; ++i) if(!nm[i]) {
+		tt = 0;
+		f(f, i, -1);
+	}
+	int ni = 0;
+	for(int i = 0; i < n; ++i) if(art[i]) {
+		id[i] = ni++;
+		t.push_back({});
+	}
+	for(auto &c : cmps) {
+		int u = ni++;
+		t.push_back({});
+		for(int v : c) {
+			if(!art[v]) id[v] = u;
+			else {
+				t[u].push_back(id[v]);
+				t[id[v]].push_back(u);
+			}
 		}
 	}
-	return top;
-}
-
-template<class F>
-void bicomps(F f) {
-	num.assign(ed.size(), 0);
-	for (int i = 0; i < (int)ed.size(); i++) if (!num[i]) dfs(i, -1, f);
+	return t;
 }
